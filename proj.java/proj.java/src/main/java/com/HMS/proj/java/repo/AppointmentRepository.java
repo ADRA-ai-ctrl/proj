95889174907b63e2e67b7faebabf4791b0590603
jhon-dev <navarro.jhonllyod2005@gmail.com>
2025-11-21 15:51:04 +0800

    aray mo

--- proj.java/proj.java/src/main/java/com/HMS/proj/java/repo/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.HMS.proj.java.repo;
-
-import com.HMS.proj.java.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment,Integer> {
-
-    void deleteById(Long id);
-}
